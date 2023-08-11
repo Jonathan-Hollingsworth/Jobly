9e2c3ccfb6da10d96cee3b4fd32a1c88af45b209
Jonathan Hollingsworth <jonathanhollingsworth07@gmail.com>
2023-08-11 13:32:22 -0400

    Documented sqlForPartialUpdate

--- helpers/sql.js
+++ helpers/sql.js
@@ -1,6 +1,20 @@
 const { BadRequestError } = require("../expressError");
 
-// THIS NEEDS SOME GREAT DOCUMENTATION.
+/** Modularly formats given data into an sql readable form for the sake of updating.
+ * 
+ * dataToUpdate: The data that is going to be formatted so it can be updated.
+ * 
+ * jsToSql: Tells the function top convert certain JS key names into their SQL counterpart.
+ * - 'firstName' becomes 'first_name' for example.  
+ * - This is done for the sake of formatting.
+ * 
+ * Input: ({ firstName: 'Aliya', age: 32 }, { firstName: "first_name" })  
+ * 
+ * Output: {  
+ * setCols: '"first_name"=$1, "age"=$2',  
+ * values: ['Aliya', 32]  
+ * }  
+ */
 
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
   const keys = Object.keys(dataToUpdate);
@@ -12,7 +26,7 @@ function sqlForPartialUpdate(dataToUpdate, jsToSql) {
   );
 
   return {
-    setCols: cols.join(", "),
+    setCols: cols.join(", "), //setCols becomes one complete string
     values: Object.values(dataToUpdate),
   };
 }
