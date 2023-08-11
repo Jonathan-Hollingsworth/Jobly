de6e4f55e3413fdbb5d611c393ea2a2e61b3ab5b
Jonathan Hollingsworth <jonathanhollingsworth07@gmail.com>
2023-08-11 14:02:29 -0400

    Written and ran tests for sqlForPartialUpdate

---
+++ helpers/sql.test.js
@@ -0,0 +1,15 @@
+const { sqlForPartialUpdate } = require ("./sql")
+const { BadRequestError } = require("../expressError");
+
+describe('sqlForPartialUpdate', () => {
+    test('should work as intended', () => {
+        const resp = sqlForPartialUpdate({ firstName: 'Aliya', age: 32 }, { firstName: "first_name" });
+        expect(resp).toEqual({setCols: '"first_name"=$1, "age"=$2', values: ['Aliya', 32]});
+    });
+
+    test('should throw BadRequestError if no data given', () => {
+        const resp = (() => {sqlForPartialUpdate({})})
+        expect(resp).toThrow(BadRequestError)
+        expect(resp).toThrow("No data")
+    })
+});
