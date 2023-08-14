7e5955376da51b8fe34f1ea280ae9b1b4d3a564d
Jonathan Hollingsworth <jonathanhollingsworth07@gmail.com>
2023-08-14 14:47:24 -0400

    Added admin authorization

--- routes/users.js
+++ routes/users.js
@@ -5,8 +5,8 @@
 const jsonschema = require("jsonschema");
 
 const express = require("express");
-const { ensureLoggedIn } = require("../middleware/auth");
-const { BadRequestError } = require("../expressError");
+const { ensureLoggedIn, ensureAdmin } = require("../middleware/auth");
+const { BadRequestError, UnauthorizedError } = require("../expressError");
 const User = require("../models/user");
 const { createToken } = require("../helpers/tokens");
 const userNewSchema = require("../schemas/userNew.json");
@@ -24,10 +24,10 @@ const router = express.Router();
  * This returns the newly created user and an authentication token for them:
  *  {user: { username, firstName, lastName, email, isAdmin }, token }
  *
- * Authorization required: login
+ * Authorization required: admin
  **/
 
-router.post("/", ensureLoggedIn, async function (req, res, next) {
+router.post("/", ensureAdmin, async function (req, res, next) {
   try {
     const validator = jsonschema.validate(req.body, userNewSchema);
     if (!validator.valid) {
@@ -48,10 +48,10 @@ router.post("/", ensureLoggedIn, async function (req, res, next) {
  *
  * Returns list of all users.
  *
- * Authorization required: login
+ * Authorization required: admin
  **/
 
-router.get("/", ensureLoggedIn, async function (req, res, next) {
+router.get("/", ensureAdmin, async function (req, res, next) {
   try {
     const users = await User.findAll();
     return res.json({ users });
@@ -70,8 +70,11 @@ router.get("/", ensureLoggedIn, async function (req, res, next) {
 
 router.get("/:username", ensureLoggedIn, async function (req, res, next) {
   try {
-    const user = await User.get(req.params.username);
-    return res.json({ user });
+    if (res.locals.user.username === req.params.username || res.locals.user.isAdmin) {
+      const user = await User.get(req.params.username);
+      return res.json({ user });
+    }
+    throw new UnauthorizedError("Only admins and appropiate user can recieve this data")
   } catch (err) {
     return next(err);
   }
@@ -96,8 +99,11 @@ router.patch("/:username", ensureLoggedIn, async function (req, res, next) {
       throw new BadRequestError(errs);
     }
 
-    const user = await User.update(req.params.username, req.body);
-    return res.json({ user });
+    if (res.locals.user.username === req.params.username || res.locals.user.isAdmin) {
+      const user = await User.update(req.params.username, req.body);
+      return res.json({ user });
+    }
+    throw new UnauthorizedError("Only admins and appropiate user can edit this user")
   } catch (err) {
     return next(err);
   }
@@ -111,8 +117,11 @@ router.patch("/:username", ensureLoggedIn, async function (req, res, next) {
 
 router.delete("/:username", ensureLoggedIn, async function (req, res, next) {
   try {
-    await User.remove(req.params.username);
-    return res.json({ deleted: req.params.username });
+    if (res.locals.user.username === req.params.username || res.locals.user.isAdmin) {
+      await User.remove(req.params.username);
+      return res.json({ deleted: req.params.username });
+    }
+    throw new UnauthorizedError("Only admins and appropiate user can delete this user")
   } catch (err) {
     return next(err);
   }
