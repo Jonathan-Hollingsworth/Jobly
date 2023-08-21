26cbac39a6748dcf2585a72c3bcf386dd80fa0d1
Jonathan Hollingsworth <jonathanhollingsworth07@gmail.com>
2023-08-21 14:41:05 -0400

    Removed useless comment

--- __tests__/models/job.test.js
+++ __tests__/models/job.test.js
@@ -66,7 +66,7 @@ describe("findAll", function () {
     });
   });
   
-//   /************************************** findBy */
+/************************************** findBy */
   
   describe('findBySalary', () => {
     test('works', async () => {
